b2d91e11fd2281cedabbd4ee1405e1c3e120214d
AreyBRSK <areybrsk@gmail.com>
2022-05-13 09:01:59 +0300

    Data Manager setup

--- SpringAnimation_Lesson_2_9/Services/DataManager.swift
+++ SpringAnimation_Lesson_2_9/Services/DataManager.swift
@@ -5,4 +5,13 @@
 //  Created by Андрей Барсук on 13.05.2022.
 //
 
-import Foundation
+import Spring
+
+class DataManager {
+    static let shared = DataManager()
+    
+    let animations = Spring.AnimationPreset.allCases
+    let curves = Spring.AnimationCurve.allCases
+    
+    init() {}
+}
