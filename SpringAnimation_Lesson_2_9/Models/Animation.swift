1a976dcde81259dc1b1acdf7b21a012d1ccd650e
AreyBRSK <areybrsk@gmail.com>
2022-05-13 09:08:05 +0300

    Model setup

--- SpringAnimation_Lesson_2_9/Models/Animation.swift
+++ SpringAnimation_Lesson_2_9/Models/Animation.swift
@@ -7,4 +7,30 @@
 
 import Foundation
 
-
+struct Animation {
+    let name: String
+    let curve: String
+    let force: Float
+    let duration: Float
+    let delay: Float
+    
+    var description: String {
+        """
+        Animation: \(name)
+        Curve: \(curve)
+        Force: \(force)
+        Duration: \(duration)
+        Delay: \(delay)
+        """
+    }
+    
+    static func getRandomAnimation() -> Animation {
+        Animation(
+            name: DataManager.shared.animations.randomElement()?.rawValue ?? "Animation name",
+            curve: DataManager.shared.animations.randomElement()?.rawValue ?? "Animation curve",
+            force: Float.random(in: 0.5...2),
+            duration: Float.random(in: 0.5...2),
+            delay: Float.random(in: 0.1...0.7)
+        )
+    }
+}
