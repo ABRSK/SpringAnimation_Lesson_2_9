a6190b146dfc13972ae6bb7f3b1373f00a941361
AreyBRSK <areybrsk@gmail.com>
2022-05-13 08:34:18 +0300

    Initial setup

---
+++ SpringAnimation_Lesson_2_9/Models/Animation.swift
@@ -0,0 +1,10 @@
+//
+//  Animation.swift
+//  SpringAnimation_Lesson_2_9
+//
+//  Created by Андрей Барсук on 13.05.2022.
+//
+
+import Foundation
+
+
---
+++ SpringAnimation_Lesson_2_9/Services/DataManager.swift
@@ -0,0 +1,8 @@
+//
+//  DataManager.swift
+//  SpringAnimation_Lesson_2_9
+//
+//  Created by Андрей Барсук on 13.05.2022.
+//
+
+import Foundation
