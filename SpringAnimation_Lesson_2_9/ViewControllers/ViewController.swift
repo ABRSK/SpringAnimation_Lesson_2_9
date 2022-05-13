c6f7d1709054c669240a0d913bd909cbaf3e173e
AreyBRSK <areybrsk@gmail.com>
2022-05-13 09:26:19 +0300

    Added ViewController logic

--- SpringAnimation_Lesson_2_9/ViewControllers/ViewController.swift
+++ SpringAnimation_Lesson_2_9/ViewControllers/ViewController.swift
@@ -5,15 +5,35 @@
 //  Created by Андрей Барсук on 13.05.2022.
 //
 
-import UIKit
+import Spring
 
 class ViewController: UIViewController {
 
+    @IBOutlet var animationView: SpringView!
+    @IBOutlet var animationLabel: UILabel!
+    @IBOutlet var playAnimationButton: UIButton!
+    
+    private var animation = Animation.getRandomAnimation()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        animationLabel.text = animation.description
+        playAnimationButton.setTitle("Play \(animation.name)", for: .normal)
     }
 
-
+    @IBAction func playAnimationButtonPressed(_ sender: UIButton) {
+        animationView.animation = animation.name
+        animationView.curve = animation.curve
+        animationView.force = CGFloat(animation.force)
+        animationView.duration = CGFloat(animation.duration)
+        animationView.delay = CGFloat(animation.delay)
+        
+        animationView.animate()
+        
+        animation = Animation.getRandomAnimation()
+        animationLabel.text = animation.description
+        sender.setTitle("Play \(animation.name)", for: .normal)
+    }
 }
 
